8fdb16a26ebdd6164bcd4c8161ece4f7eb69ef3b
Gashwa Web 1 <web.gashwatechnologies1@gmail.com>
2025-12-11 17:29:24 +0530

    newxjna adsadsad

--- app/api/auth/route.js
+++ app/api/auth/route.js
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
