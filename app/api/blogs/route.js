7444a1d0f9878eff7526486e29f45b6341e2087d
Gashwa Web 1 <web.gashwatechnologies1@gmail.com>
2025-12-12 09:20:20 +0530

    solving

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import slugify from "slugify";
 
-const prisma = new PrismaClient();
+import { prisma } from "@/lib/prisma";
 
 export async function POST(req) {
   const body = await req.json();
